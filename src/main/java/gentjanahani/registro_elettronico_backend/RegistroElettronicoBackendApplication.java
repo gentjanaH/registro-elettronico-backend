e2708477495142855bf8fc4e1d6efca57c332aa9
Gentjana <hanigentjana@gmail.com>
2026-03-11 14:50:55 +0100

    add_progetto-capstone

---
+++ src/main/java/gentjanahani/registro_elettronico_backend/RegistroElettronicoBackendApplication.java
@@ -0,0 +1,13 @@
+package gentjanahani.registro_elettronico_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroElettronicoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroElettronicoBackendApplication.class, args);
+	}
+
+}
