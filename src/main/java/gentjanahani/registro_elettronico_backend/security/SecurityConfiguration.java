5dae896d0e06bbcb09542826432c1fb75c8eb699
Gentjana <hanigentjana@gmail.com>
2026-03-14 21:28:02 +0100

    add-cors

--- src/main/java/gentjanahani/registro_elettronico_backend/security/SecurityConfiguration.java
+++ src/main/java/gentjanahani/registro_elettronico_backend/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package gentjanahani.registro_elettronico_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 //questa classe mi serve per configurare la Security Filter Chain (sequenza di filtri che vengono eseguiti
@@ -30,7 +36,7 @@ public class SecurityConfiguration {
 
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
 
-
+        httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
@@ -38,5 +44,20 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder(12);//numeri di round
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://mywonderfulfe.com"));
+        // Qua definisco una WHITELIST di uno o più indirizzi FRONTEND che voglio possano accedere a questo backend senza i problemi di CORS
+        // Volendo ma rischioso si potrebbe anche mettere '*' però questo permetterebbe l'accesso a tutti (utile solo nel caso di API pubbliche)
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
